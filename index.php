cca416e0d0206ecb78fafd1fdc60d4a6072885a9
Christopher Collopy <chelco91@gmail.com>
2015-07-24 12:13:12 -0600

    edited the landing index.php file

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Food Inventory</title>
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body>
+		<div class="container">
+			<header>
+				<h1><strong>Capstone: </strong>Food Inventory</h1>
+				<nav>
+					<ul>
+						<li><a class="" href="#">Home</a></li>
+						<li><a href="./epic/documentation.php">EPIC</a></li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<div class="section">
+					<h2>Welcome to our site!</h2>
+					<p>A place to showcase my web programming skills as well as to network with people I meet.</p>
+				</div>
+				<div class="section">
+					<h2></h2>
+					<p></p>
+				</div>
+				<div class="section">
+					<h2></h2>
+					<p></p>
+				</div>
+			</main>
+		</div>
+	</body>
+</html>
