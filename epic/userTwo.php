ef0293f74a06177d764e95cfb4e72a317c67e5e6
Marie Vigil <mjkvigil@gmail.com>
2015-07-26 20:50:23 -0600

    start user case two html code created "images" directory added "diagram" class to CSS placed "warehouse-diagram.jpg" image into images directory

--- epic/userTwo.php
+++ epic/userTwo.php
@@ -1,1 +1,35 @@
-//code here
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+		<head lang="en">
+			<meta charset="UTF-8">
+
+
+	<head>
+		<title>User Persona&mdash;Warehouse/Backstock Manager/Clerk</title>
+		<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+	</head>
+
+
+	<body>
+
+
+		<h1>User Persona&mdash;Warehouse/Backstock Manager/Clerk</h1>
+
+		<div>
+		<img class="diagram" src="images/warehouse-diagram.jpg" alt="warehouse-diagram" title="warehouse-diagram" />
+		<p><small>Resource: http://log.logcluster.org/mobile/response/warehouse-management/index.html</small></p>
+		</div>
+
+		<p></p>
+
+
+
+
+
+
+
+
+
+
+	</body>
+</html>
