1d863401062fcf1428eb71a179904ce25f57b182
Charles Sandidge <sandidgec@gmail.com>
2015-07-27 14:45:45 -0600

    added wireframes page and links

--- epic/wireframes.php
+++ epic/wireframes.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-	<head lang="en">
-		<meta charset="UTF-8">
-
-
+<html>
 	<head>
-		<title>User Persona&mdash;Warehouse/Backstock Clerk</title>
-		<link href="styles.css" rel="stylesheet" type="text/css" media="all" />
-		<nav>
-			<ul>
-				<li><a href="../index.php">Home</a></li>
-				<li><a href="./documentation.php">Epic</a></li>
-				<li><a href="./persona1.php">Persona 1</a></li>
-				<li><a href="./persona2.php">Persona 2</a></li>
-				<li><a href="./wireframes.php">Wireframes</a></li>
-			</ul>
-		</nav>
+		<meta charset="UTF-8" />
+		<title>Deanna, Small Business Owner</title>
+		<link rel="stylesheet" type="text/css" href="../css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="../css/personas.css" />
 	</head>
-
-
 	<body>
-
-		<img src="../images/wireframe-inventory1.jpg" alt="inventory text wireframe"
+		<div class="container">
+			<header>
+				<h1>
+					Wireframes
+				</h1>
+				<nav>
+					<ul>
+						<li><a href="../index.php">Home</a></li>
+						<li><a href="./documentation.php">Epic</a></li>
+						<li><a href="./persona2.php">Persona 2</a></li>
+						<li><a href="./persona3.php">Persona 3</a></li>
+						<li><a href="./wireframes.php">Wireframes</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+		<img src="../images/wireframe-inventory1.jpg" alt="wireframe">
 	</body>
 </html>
