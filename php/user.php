0c7b7589ccbdf41effdce8c5ea7fe8d01ee5d424
Charles Sandidge <sandidgec@gmail.com>
2015-07-28 16:40:48 -0600

    added wireframes page and links

---
+++ php/user.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sandidgec
+ * Date: 7/28/2015
+ * Time: 4:01 PM
+ */
+
+class User {
+
+	/**
+	 * id for this user; this is the primary key
+	 * @var int $userId
+	 **/
+	private $userId;
+	/**
+	 *
+	 *
+	 **/
+	private $lastName;
+	/**
+	 *
+	 *
+	 **/
+	private $firstName;
+	/**
+	 *
+	 *
+	 **/
+	private $email;
+	/**
+	 *
+	 *
+	 **/
+	private $phoneNumber;
+	/**
+	 *
+	 *
+	 **/
+	private $password;
