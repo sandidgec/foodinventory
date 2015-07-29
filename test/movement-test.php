3ebda37093131a64dd64fdc16368f8585dbab3b9
Christopher Collopy <chelco91@gmail.com>
2015-07-29 15:17:43 -0600

    created test directory and added a movement-test.php page https://app.asana.com/0/42983347910373/42983347910387

---
+++ test/movement-test.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("../php/movement.php");
+
+try {
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/invtext.ini");
+	$movement = new Movement(null, 1, 1, 1, 1, 2.50, "2015-07-29", "T", 4.50);
+	$movement->insert($pdo);
+	var_dump($movement);
+} catch(PDOException $pdoException) {
+	// handle PDO errors
+} catch(Exception $exception) {
+	// handle other errors
+}
+
+$movement_dump = Movement::getAllMovements($pdo);
+
+foreach($movement_dump as $index) {
+	echo $index;
+}
+
+?>
