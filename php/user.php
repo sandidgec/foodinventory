20c272cd40dd72a395077475ef93eef00d73f3c1
Charles Sandidge <sandidgec@gmail.com>
2015-07-29 14:00:59 -0600

    built accessor / mutator for state variables :  https://app.asana.com/0/42983347910373/42983347910385

--- php/user.php
+++ php/user.php
@@ -8,30 +8,82 @@ class User {
 	 **/
 	private $userId;
 	/**
-	 *
-	 *
+	 * lastName of userId
+	 * @var string $lastName
 	 **/
 	private $lastName;
 	/**
-	 *
-	 *
+	 * firstName of userId
+	 * @var string $firstName
 	 **/
 	private $firstName;
 	/**
-	 *
-	 *
+	 * email of userId
+	 * @var string $email
 	 **/
 	private $email;
 	/**
-	 *
-	 *
+	 * phoneNumber of userId
+	 * @var int $phoneNumber
 	 **/
 	private $phoneNumber;
 	/**
-	 *password hash for userId;
+	 * password hash for userId;
 	 * @var string $passwordHash
 	 **/
-	private $passwordHash;
+	private $hash;
+	/**
+	 * password salt for userId;
+	 * @var string $passwordSalt
+	 */
+	private $salt;
 
 
+	/**
+	 * accessor method for profile id
+	 *
+	 * @return int value of profile id
+	 **/
+
+	public function getUserId() {
+		return ($this->userId);
+	}
+
+	/** mutator method for profile id
+	 *
+	 * @throws InvalidArgumentException if $userId is not a int
+	 */
+
+	public function setUserId($newUserId) {
+		//verify the User is valid
+		$newUserId = filter_var($newUserId, FILTER_VALIDATE_INT);
+		if(empty($newUserId) === true) {
+			throw (new InvalidArgumentException ("content invalid"));
+		}
+	}
+	/**
+	 * accessor method for Last Name
+	 *
+	 */
+
+	public function getLastName() {
+		return ($this->lastName);
+	}
+
+	/**
+	 * Mutator for last name sanitation
+	 * @param $newLastName
+	 */
+	public function setLastName($newLastName) {
+		//verify last name is valid
+	$newLastName = filter_var($newLastName, FILTER_SANITIZE_STRING);
+		if(empty($newLastName) === true) {
+			throw new InvalidArgumentException("content invalid");
+		}
+		if(strlen($newLastName)> 32){
+			throw new RangeException("Last Name content too large");
+		}
+
+		$this->lastName = $newLastName;
+	}
 }
