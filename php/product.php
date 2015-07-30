522c0fcd5506b2bd36c5adc9f6e57cd2d973eeca
Marie Vigil <mjkvigil@gmail.com>
2015-07-30 15:33:30 -0600

    moved product and product_permissions php files into php directory

---
+++ php/product.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * product class for foodinventory
+ *
+ * This product class is a single class within the overall product for foodinventory
+ *
+ * @author Marie Vigil <marie@jtdesignsolutions>
+ *
+ **/
+
+class Product {
+	/**
+	 * id for this Product; this is the primary key
+	 * @var int $productId
+	 **/
+	private $productId;
+	/**
+	 * id for the Description of the product; this is a foreign key
+	 * @var int $descriptionId
+	 **/
+	private $descriptionId;
+	/**
+	 * actual textual content of this Tweet
+	 * @var string $tweetContent
+	 **/
+
+
+
+
+
+?>
---
+++ php/product_permissions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * product_permissions class for foodinventory
+ *
+ * This product class is a single class within the overall product for foodinventory
+ *
+ * @author Marie Vigil <marie@jtdesignsolutions>
+ *
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
