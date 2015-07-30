20b81b5832ecb680965c835d73adff479fea48b3
Marie Vigil <mjkvigil@gmail.com>
2015-07-30 16:03:32 -0600

    https://app.asana.com/0/42983347910373/42983347910398 writing constructor for the Product Id and Description Id question about throw and catch if not matching valid id constraints

--- php/product.php
+++ php/product.php
@@ -20,7 +20,30 @@ class Product {
 	 **/
 	private $descriptionId;
 
-
+	/**
+	 * constructor for this Product
+	 *
+	 * @param mixed $newProductId id of this Product or null if a new Product
+	 * @param int $newDescriptionId id of the Description for this Product
+	 * @throws InvalidArgumentException if data types are not valid
+	 * @throws RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws Exception if some other exception is thrown
+	 **/
+	public function __construct($newProductId, $newDescriptionId = null) {
+		try {
+			$this->setProductId($newProductId);
+			$this->setDescriptionId($newDescriptionId);
+		} catch(InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new RangeException($range->getMessage(), 0, $range));
+		} catch(Exception $exception) {
+			// rethrow generic exception
+			throw(new Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 
 
 
