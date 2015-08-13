fafe697691f051d25789ff1e5f1435922969a40a
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 11:59:58 -0600

    https://app.asana.com/0/41180108506105/45375877950174
    lunch time :-)

--- backend/php/api/product/index.php
+++ backend/php/api/product/index.php
@@ -1,9 +1,10 @@
 <?php
-require_once(dirname(__DIR__) . "/foodinventory/backend/php/api/product/index.php");
-//require_once(dirname(dirname(dirname(__DIR__))) . "/foodinventory/backend/php/api/product/xsrf.php");
-require_once(dirname(dirname(dirname(__DIR__))) . "/foodinventory/backend/php/api/product/.htaccess.php");
-//require_once("/etc/apache2/data-design/encrypted-config.php");
+require_once(dirname(__DIR__) . "/backend/php/api/product/index.php");
+//require_once(dirname(__DIR__) . "/backend/php/api/product/xsrf.php");
+//require_once((dirname(__DIR__) . "/backend/php/api/product/.htaccess.php");
 
+
+//require_once("/etc/apache2/data-design/encrypted-config.php");
 // start the session and create a XSRF token
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
@@ -18,24 +19,24 @@ try {
 	// determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	// sanitize the id
-	$productid = filter_input(INPUT_GET, "product id", FILTER_VALIDATE_INT);
-	if(($method === "DELETE" || $method === "PUT") && (empty($productid) === true || $productid < 0)) {
+	// sanitize the product id
+	$productId = filter_input(INPUT_GET, "product id", FILTER_VALIDATE_INT);
+	if(($method === "DELETE" || $method === "PUT") && (empty($productId) === true || $productId < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
 	// grab the mySQL connection
 	$pdo = connectToEncryptedMySql("/foodinventory/backend/sql/invtext.sql");
 
-	// handle all RESTful calls toProduct
+	// handle all RESTful calls to Product
 	// get some or all Products
 	if($method === "GET") {
 		// set an XSRF cookie on GET requests
 		setXsrfCookie("/");
-		if(empty($productid) === false) {
-			$reply->data = Product::getProductByProductId($pdo, $productid);
+		if(empty($productId) === false) {
+			$reply->data = Product::getProductByProductId($pdo, $productId);
 		} else {
-			$reply->data = Product::getAllProdcuts($pdo)->toArray();
+			$reply->data = Product::getAllProducts($pdo)->toArray();
 		}
 		// put to an existing Product
 	} else if($method === "PUT") {
@@ -44,7 +45,7 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		$product = new Products($productid, $requestObject->pagination,  $requestObject->title, $requestObject->vendor, $requestObject->discription);
+		$product = new Products($requestObject->pagination,  $requestObject->title, $requestObject->vendor, $requestObject->discription);
 		$product->update($pdo);
 		$reply->data = "Product updated OK";
 	}
@@ -62,7 +63,7 @@ try {
 	// delete an existing Product
 	else if($method === "DELETE") {
 	verifyXsrf();
-	$product = Product::getProductByProductId($pdo, $productid);
+	$product = Product::getProductByProductId($pdo, $productId);
 	$product->delete($pdo);
 	$reply->data = "Product deleted OK";
 	}
