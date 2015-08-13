e12133ec27b7d69b0ac9febe571c231c149002ce
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 11:45:26 -0600

    https://app.asana.com/0/41180108506105/45375877950174
    deleted extra curly braces

--- backend/php/api/product/index.php
+++ backend/php/api/product/index.php
@@ -20,7 +20,7 @@ try {
 
 	// sanitize the id
 	$productid = filter_input(INPUT_GET, "product id", FILTER_VALIDATE_INT);
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $productid < 0)) {
+	if(($method === "DELETE" || $method === "PUT") && (empty($productid) === true || $productid < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
@@ -33,7 +33,7 @@ try {
 		// set an XSRF cookie on GET requests
 		setXsrfCookie("/");
 		if(empty($productid) === false) {
-			$reply->data = Product::getProductByProductId($pdo, $id);
+			$reply->data = Product::getProductByProductId($pdo, $productid);
 		} else {
 			$reply->data = Product::getAllProdcuts($pdo)->toArray();
 		}
@@ -44,30 +44,30 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		$product = new TweetProduct($productid, $requestObject->pagination,  $requestObject->title, $requestObject->vendor, $requestObject->discription);
-		$tweet->update($pdo);
-		$reply->data = "Tweet updated OK";
+		$product = new Products($productid, $requestObject->pagination,  $requestObject->title, $requestObject->vendor, $requestObject->discription);
+		$product->update($pdo);
+		$reply->data = "Product updated OK";
 	}
-	// post to a new Tweet
-} else if($method === "POST") {
+	// post to a new Product
+	else if($method === "POST") {
 	// convert POSTed JSON to an object
 	verifyXsrf();
 	$requestContent = file_get_contents("php://input");
 	$requestObject = json_decode($requestContent);
 
-	$tweet = new Tweet(null, $requestObject->profileId, $requestObject->tweetContent, $requestObject->tweetDate);
-	$tweet->insert($pdo);
-	$reply->data = "Tweet created OK";
-}
-	// delete an existing Tweet
-	} else if($method === "DELETE") {
+	$product = new Product(null, $requestObject->productId, $requestObject->pagination,  $requestObject->title, $requestObject->vendor, $requestObject->discription);
+	$product->insert($pdo);
+	$reply->data = "Product created OK";
+	}
+	// delete an existing Product
+	else if($method === "DELETE") {
 	verifyXsrf();
-	$tweet = Tweet::getTweetByTweetId($pdo, $id);
-	$tweet->delete($pdo);
-	$reply->data = "Tweet deleted OK";
-}
+	$product = Product::getProductByProductId($pdo, $productid);
+	$product->delete($pdo);
+	$reply->data = "Product deleted OK";
+	}
 // create an exception to pass back to the RESTful caller
-} catch(Exception $exception) {
+	} catch(Exception $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 	unset($reply->data);
