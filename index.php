eafa4abdd70b35492ea76d2d0e0e9bc9b1007832
Christopher Collopy <chelco91@gmail.com>
2015-08-13 13:15:35 -0600

    updated front landing page

--- index.php
+++ index.php
@@ -1,38 +1,64 @@
 <!DOCTYPE html>
-<html>
-	<head lang="en">
-		<meta charset="UTF-8" />
-		<title>Inventory Text</title>
-		<link rel="stylesheet" type="text/css" href="lib/css/styles.css" />
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- LINK TO YOUR CUSTOM CSS FILES HERE -->
+		<link rel="stylesheet" href="lib/css/index-style.css" />
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+		<title>Inventory Text -> Craft Clerk</title>
 	</head>
-	<body>
-		<div class="container">
-			<header>
-				<h1>
-					<strong>Capstone: </strong>
-						"Inventory Text"
-				</h1>
-				<nav>
-					<ul>
-						<li><a href="#" class="active">Home</a></li>
-						<li><a href="epic/documentation.php">Epic</a></li>
-					</ul>
+	<body class="sfooter">
+
+		<!--this wrapper div is for the sticky footer-->
+		<div class="sfooter-content">
+
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<a class="navbar-brand" href="#"><img alt="Brand" src="lib/images/sponge-brand.png" height="20" /></a>
+							<p class="navbar-text title">Capstone: <strong class="strike">Inventory Text</strong> <strong>Craft Clerk</strong></p>
+						</div>
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav pull-right">
+								<li role="presentation"><a href="#" class="active">Home</a></li>
+								<li role="presentation"><a href="epic/documentation.php">Epic</a></li>
+							</ul>
+						</div>
+					</div>
 				</nav>
-			</header>
-			<main>
+			</div>
+			<main class="container">
 				<div class="section">
 					<h2>Welcome to our site!</h2>
 					<p>
-						<img src="lib/images/phoneExample.png" alt="phone image">
+						<img src="lib/images/phoneExample.png" alt="phone image" width="600">
 					</p>
-				</div>
-				<div class="section">
-					<h2></h2>
-					<p></p>
-				</div>
-				<div class="section">
-					<h2></h2>
-					<p></p>
 				</div>
 			</main>
 		</div>
