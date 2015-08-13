9634094f8ed3f42e2d47c0b40df6fa1f20a9586d
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 16:14:37 -0600

    https://app.asana.com/0/41180108506105/45375877950174
    require_once(dirname(dirname(__DIR__)) . "/classes/autoload.php");

--- backend/php/api/product/index.php
+++ backend/php/api/product/index.php
@@ -1,6 +1,7 @@
 <?php
-require_once(dirname(__DIR__) . "//etc/apache2/capstone/lib/xsrf.php");
-require_once(dirname(__DIR__) . "/etc/apache2/capstone/.htaccess.php");
+require_once(dirname(dirname(__DIR__)) . "/classes/autoload.php");
+require_once(dirname(dirname(__DIR__)) . "/lib/xsrf.php");
+require_once("/etc/apache2/data-design/encrypted-config.php");
 
 // start the session and create a XSRF token
 if(session_status() !== PHP_SESSION_ACTIVE) {
