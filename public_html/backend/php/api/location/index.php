e9116adf3cae2410609b66e6457f75ae2e41dc13
Charles Sandidge <sandidgec@gmail.com>
2015-08-19 16:02:35 -0600

    updated Location APIT test, passed   https://app.asana.com/0/42983347910373/45375877950165

--- public_html/backend/php/api/location/index.php
+++ public_html/backend/php/api/location/index.php
@@ -21,75 +21,72 @@ try {
 	$locationId = filter_input(INPUT_GET, "locationId", FILTER_VALIDATE_INT);
 	if(($method === "DELETE" || $method === "PUT") && (empty($locationId) === true || $locationId < 0)) {
 		throw(new InvalidArgumentException("locationId cannot be empty or negative", 405));
-
-	// sanitize the locationId
-	$locationId = filter_input(INPUT_GET, "locationId", FILTER_VALIDATE_INT);
-
-	// sanitize the storageCode
-	$storageCode = filter_input(INPUT_GET, "storageCode", FILTER_SANITIZE_STRING);
-
-	// sanitize getProducts
-	$getProducts = filter_input(INPUT_GET, "getProducts", FILTER_VALIDATE_BOOLEAN);
-
-
-
-	// grab the mySQL connection
-	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/invtext.ini");
-
-	// handle the RESTful calls to location
-	// get some or all locations
-	if($method === "GET") {
-		// set an XSRF cookie on GET requests
-		setXsrfCookie("/");
-		if(empty($locationId) === false) {
-			if($getProducts === true) {
-				$reply->data = Location::getProductByLocationId($pdo, $locationId);
-			} else {
-				$reply->data = Location::getLocationByLocationId($pdo, $locationId);
-			}
-		} else if(empty($storageCode) === false) {
-			$reply->data = Location::getLocationByStorageCode($pdo, $storageCode);
-		} else{
-			$reply->data = Location::getALLLocations($pdo);
-		}
-
-		// post to a new Location
-	} else if($method === "POST") {
-		// convert POSTed JSON to an object
-		verifyXsrf();
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
-
-		$location = new Location( $locationId, $requestObject->storageCode, $requestObject->description);
-		$location->insert($pdo);
-		$reply->data = "Location created OK";
-
-
-		// delete an existing Location
-	} else if($method === "DELETE") {
-		verifyXsrf();
-		$location = Location::getLocationByLocationId($pdo, $locationId);
-		$location->delete($pdo);
-		$reply->data = "Location deleted OK";
-
-	// put to an existing Location
-	} else if($method === "PUT") {
-		// convert PUTed JSON to an object
-		verifyXsrf();
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
-
-		$location = new Location($locationId, $requestObject->storageCode, $requestObject->description);
-		$location->update($pdo);
-		$reply->data = "Location Updated Ok";
 	}
 
+		// sanitize the storageCode
+		$storageCode = filter_input(INPUT_GET, "storageCode", FILTER_SANITIZE_STRING);
+
+		// sanitize getProducts
+		$getProducts = filter_input(INPUT_GET, "getProducts", FILTER_VALIDATE_BOOLEAN);
+
+
+		// grab the mySQL connection
+		$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/invtext.ini");
+
+		// handle the RESTful calls to location
+		// get some or all locations
+		if($method === "GET") {
+			// set an XSRF cookie on GET requests
+			setXsrfCookie("/");
+			if(empty($locationId) === false) {
+				if($getProducts === true) {
+					$reply->data = Location::getProductByLocationId($pdo, $locationId);
+				} else {
+					$reply->data = Location::getLocationByLocationId($pdo, $locationId);
+				}
+			} else if(empty($storageCode) === false) {
+				$reply->data = Location::getLocationByStorageCode($pdo, $storageCode);
+			} else {
+				$reply->data = Location::getALLLocations($pdo);
+			}
+
+			// post to a new Location
+		} else if($method === "POST") {
+			// convert POSTed JSON to an object
+			verifyXsrf();
+			$requestContent = file_get_contents("php://input");
+			$requestObject = json_decode($requestContent);
+
+			$location = new Location($locationId, $requestObject->storageCode, $requestObject->description);
+			$location->insert($pdo);
+			$reply->data = "Location created OK";
+
+
+			// delete an existing Location
+		} else if($method === "DELETE") {
+			verifyXsrf();
+			$location = Location::getLocationByLocationId($pdo, $locationId);
+			$location->delete($pdo);
+			$reply->data = "Location deleted OK";
+
+			// put to an existing Location
+		} else if($method === "PUT") {
+			// convert PUTed JSON to an object
+			verifyXsrf();
+			$requestContent = file_get_contents("php://input");
+			$requestObject = json_decode($requestContent);
+
+			$location = new Location($locationId, $requestObject->storageCode, $requestObject->description);
+			$location->update($pdo);
+			$reply->data = "Location Updated Ok";
+		}
+
 // create an exception to pass back to the RESTful caller
-} 	catch(Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-	unset($reply->data);
-}
+} catch(Exception $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
+		unset($reply->data);
+	}
 
 header("Content-type: application/json");
 echo json_encode($reply);
