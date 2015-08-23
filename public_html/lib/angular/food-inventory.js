9598ca9898490a4034ea7a14ba26154eaa98e35d
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 14:31:57 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    working on "food-inventory".js within public_html/lib/angular
    
    food-inventory.js will act as an angular Router to all parts of angular web app

--- public_html/lib/angular/food-inventory.js
+++ public_html/lib/angular/food-inventory.js
@@ -1,1 +1,27 @@
+/**
+ * creating app.config $routeProvider angular function
+ * this angular.module acts as a Router to other angular modules
+ * in the food inventory web application
+ *
+ * Note that each separate function of web app will need to connect here
+ *
+ * created by Marie on Sunday 8/23/15
+ */
+
+
+
+function angularModule() {
+// wrapping angular.module within a javaScript function
+
+
+
 var app = angular.module("FoodInventory", ["ngMessage", "ui.bootstrap"]);
+
+
+
+
+
+
+
+
+}
