04a809c13019131c388f018f43dc99cd7cc61739
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 15:35:53 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    started putting code in productsCtrl file

---
+++ public_html/lib/angular/controllers/productsCtrl.js
@@ -0,0 +1,18 @@
+/**
+ * creating app.controller function for products.php and product-edit.php
+ * in the food inventory web application
+ *
+ *
+ * file started by Marie on Sunday 8/23/15—
+ * may need revisions based on
+ * other team members' code
+ */
+
+
+
+function angularModule() {
+// wrapping angular.module within a javaScript function
+
+	var app = angular.module("", []);
+
+}
