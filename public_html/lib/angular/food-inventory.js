58e0d48c0038c430fe0e945b40ef8b1ad7efc73c
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 14:44:00 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    $routeProvider.—creating $routeProvider definition to match using ng-template reference
    
    .otherwise—set route definition that will be used on route change when no other route definition matches

--- public_html/lib/angular/food-inventory.js
+++ public_html/lib/angular/food-inventory.js
@@ -5,18 +5,26 @@
  *
  * Note that each separate function of web app will need to connect here
  *
- * created by Marie on Sunday 8/23/15
+ * file started by Marie on Sunday 8/23/15
  */
 
 
 
 function angularModule() {
 // wrapping angular.module within a javaScript function
-
-
-
-var app = angular.module("FoodInventory", ["ngMessage", "ui.bootstrap"]);
-
+var app = angular.module("FoodInventory", ["ngRoute", "ui.bootstrap","ngMessage", "ngAnimate"]);
+	app.config(["$routeProvider",
+	function($routeProvider){
+		$routeProvider.
+			when("/",{
+				//creating $routeProvider definition to match using ng-template reference
+				title:"Products",
+				templateUrl: "partials/products.php",
+				controller:"productsCtrl"
+			})
+			//set route definition that will be used on route change when no other route definition matches
+			.otherwise
+	}])
 
 
 
