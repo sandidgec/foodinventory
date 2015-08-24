5b0e030a92f8121efd2d3d84d0d3075111db1480
Marie Vigil <mjkvigil@gmail.com>
2015-08-24 16:14:16 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    updated Product service

--- public_html/lib/angular/services/product.js
+++ public_html/lib/angular/services/product.js
@@ -1,177 +1,269 @@
-/**
- * service for products
- * in the food inventory web application
- */
+app.service("ProductEditorService", function($http, $q) {
+	this.PRODUCTEDITOR_ENDPOINT = "../../backend/php/api/product/";
 
-app.service("RegisterService", function($http, $q) {
-	this.REGISTER_ENDPOINT = "../../backend/php/api/product/";
-
-//gets the Product by productId
-	this.getProductByProductId = function(productId) {
-		return ($http.get(this.REGISTER_ENDPOINT +  productId)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function() {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Product by vendorId
-	this.getProductByVendorId = function(VendorId) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?VendorId=" + VendorId)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by productId
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(productId) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + productId)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Product by description
-	this.getProductByDescription = function(description) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?description=" + description)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by vendorId
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(vendorId) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?vendorId=" + vendorId)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Product by leadTime
-	this.getProductByLeadTime = function(leadTime) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?leadTime=" + leadTime)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by description
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(description) {
+		return($http.get(this.PRODUCTTEDITOR_ENDPOINT + "?description=" + description)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Product by sku
-	this.getProductBySku = function(sku) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?sku=" + sku)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by leadTime
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(leadTime) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?leadTime=" + leadTime)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Product by title
-	this.getProductByTitle = function(title) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?title=" + title)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by sku
+	 *
+	 * @returns accepts the promise when movements are found, rejected otherwise
+	 **/
+	this.getProducts = function(sku) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?sku=" + sku)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the Location by productId
-	this.getLocationByProductId = function(productId) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?productId=" + productId)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by title
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(title) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?title=" + title)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the UnitOfMeasure by productId
-	this.getUnitOfMeasureByProductId = function(productId) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?productId=" + productId)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by location
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(location) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?location=" + location)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
-//gets the FinishedProduct by productId
-	this.getFinishedProductByProductId = function(productId) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?productId=" + productId)
-			.then(function(reply) {
+	/**
+	 * method that promises to get products by UnitOfMeasure
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(unitOfMeasure) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?unitOfMeasure=" + unitOfMeasure)
+			.then(function(reply){
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
-
-//gets the Notification by productId
-	this.getNotificationByProductId = function(productId) {
-		return ($http.get(this.REGISTER_ENDPOINT + "?productId=" + productId)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
-
-//gets the All Products
-	this.getAllProducts = function() {
-		return ($http.get(this.REGISTER_ENDPOINT)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 
 
-//post the Product
+	/**
+	 * method that promises to get products by FinishedProduct
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(finishedProduct) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?finishedProduct=" + finishedProduct)
+			.then(function(reply){
+				if(typeof reply.data === "object") {
+					return(reply.data);
+				} else {
+					return($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return($q.reject(reply.data));
+			}));
+	};
+
+	/**
+	 * method that promises to get products by Notification
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(notification) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?notification=" + notification)
+			.then(function(reply){
+				if(typeof reply.data === "object") {
+					return(reply.data);
+				} else {
+					return($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return($q.reject(reply.data));
+			}));
+	};
+
+	/**
+	 * method that promises to get products by Notification
+	 *
+	 * @returns accepts the promise when products are found, rejected otherwise
+	 **/
+	this.getProducts = function(notification) {
+		return($http.get(this.PRODUCTEDITOR_ENDPOINT + "?notification=" + notification)
+			.then(function(reply){
+				if(typeof reply.data === "object") {
+					return(reply.data);
+				} else {
+					return($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return($q.reject(reply.data));
+			}));
+	};
+
+	/**
+	 * method that promises to add a product
+	 *
+	 * @returns accepts the promise when the product is added, rejected otherwise
+	 **/
 	this.addProduct = function(product) {
-		return ($http.post(this.REGISTER_ENDPOINT, product)
+		return($http.post(this.ProductEDITOR_ENDPOINT + product)
 			.then(function(reply) {
 				if(typeof reply.data === "object") {
-					return (reply.data);
+					return(reply.data);
 				} else {
-					return ($q.reject(reply.data));
+					return($q.reject(reply.data));
 				}
 			}, function(reply) {
-				return ($q.reject(reply.data));
+				return($q.reject(reply.data));
 			}));
 	};
 });
+
+/**
+ * method that promises to edit a product
+ *
+ * @returns accepts the promise when the product is edited, rejected otherwise
+ **/
+this.editProduct = function(product) {
+	return ($http.put(this.REGISTER_ENDPOINT + product.productId, product)
+		.then(function(reply) {
+			if(typeof reply.data === "object") {
+				return (reply.data);
+			} else {
+				return ($q.reject(reply.data));
+			}
+		}, function(reply) {
+			return ($q.reject(reply.data));
+		}));
+};
+
+this.deleteProduct = function(user) {
+	return ($http.delete(this.REGISTER_ENDPOINT + product.productId)
+		.then(function(reply) {
+			if(typeof reply.data === "object") {
+				return (reply.data);
+			} else {
+				return ($q.reject(reply.data));
+			}
+		}, function(reply) {
+			return ($q.reject(reply.data));
+		}));
+};
