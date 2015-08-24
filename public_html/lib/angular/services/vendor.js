1299ad56d5106904747872aac23583dfa50ced4d
James Huber <jhuber8@cmn.edu>
2015-08-24 17:11:21 -0500

    doc blocks not cock blocks (service) https://app.asana.com/0/42983347910373/46061359778948

--- public_html/lib/angular/services/vendor.js
+++ public_html/lib/angular/services/vendor.js
@@ -1,7 +1,11 @@
 app.service("RegisterService", function($http, $q) {
 	this.REGISTER_ENDPOINT = "../../backend/php/api/vendor/";
 
-
+	/**
+	 * method that promises to get vendors by vendorId
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
 	this.getVendorByVendorId = function(vendorId) {
 		return ($http.get(this.REGISTER_ENDPOINT +  vendorId)
 			.then(function(reply) {
@@ -14,7 +18,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+	/**
+	 * method that promises to get vendors by vendorEmail
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
 	this.getVendorByVendorEmail = function(vendorEmail) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?vendorEmail=" + vendorEmail)
 			.then(function(reply) {
@@ -27,6 +35,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	 * method that promises to get vendors by vendorName
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
 	this.getVendorByVendorName = function(vendorName) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?vendorName=" + vendorName)
 			.then(function(reply) {
@@ -39,7 +52,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+	/**
+	 * method that promises to get vendors
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
 	this.getAllVendors = function() {
 		return ($http.get(this.REGISTER_ENDPOINT)
 			.then(function(reply) {
@@ -52,7 +69,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+	/**
+	 * method that promises to add vendors
+	 *
+	 * @return accepts the promise when vendors are added, rejected otherwise
+	 **/
 	this.addVendor = function(vendor) {
 		return ($http.post(this.REGISTER_ENDPOINT, vendor)
 			.then(function(reply) {
@@ -65,7 +86,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+	/**
+	 * method that promises to edit vendors
+	 *
+	 * @return accepts the promise when vendors are edited, rejected otherwise
+	 **/
 	this.editVendor = function(vendor) {
 		return ($http.put(this.REGISTER_ENDPOINT + vendor.vendorId, vendor)
 			.then(function(reply) {
@@ -78,8 +103,12 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
-	this.deleteVendor = function(user) {
+	/**
+	 * method that promises to delete vendors
+	 *
+	 * @return accepts the promise when vendors are deleted, rejected otherwise
+	 **/
+	this.destroyVendor = function(user) {
 		return ($http.delete(this.REGISTER_ENDPOINT + vendor.vendorId)
 			.then(function(reply) {
 				if(typeof reply.data === "object") {
