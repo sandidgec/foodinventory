36c2a1093db9f3462801bca0dfe056f4ed358c61
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 22:01:32 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    created lib directory in api directory
    
    created views directory in api/lib
    
    created product-view.php file
    
    views are php documents not js documents
    
    created set.php file —researched that set.php subclass recommended to  implement\ArrayAccess, \Countable, \IteratorAggregate

---
+++ public_html/backend/php/api/lib/set.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * subclass set to aid as a helper for data key retrieval within Product View
+ *
+ * It's recommended to have a subclass Set which implements
+ * \ArrayAccess, \Countable, \IteratorAggregate
+ *
+ */
+
+
+class View {}
