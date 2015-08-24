24f6fc9139c491708a4d9ba79a17a56a28b3dcba
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 20:55:13 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    product-data.js
    
    created app.product using $http service
    
    retrieve api data information for products using api index.php document
    
    Get object and return $http.get
    
    Post object and return $http.get
    
    Put object and return $http.get
    
    Delete object and return results

---
+++ public_html/lib/angular/services/product-data.js
@@ -0,0 +1,58 @@
+/**
+ * creating service for products
+ * in the food inventory web application
+ *
+ *
+ * file started by Marie on Sunday 8/23/15—
+ * may need revisions based on
+ * other team members' code
+ */
+
+
+function angularModule() {
+// wrapping angular.module within a javaScript function
+
+	var app = angular.module("", []);
+
+	// create app.product using $http service
+	//angular.js documentation—$http service is a core Angular service that facilitates communication with the remote HTTP servers via the browser's XMLHttpRequest object or via JSONP.
+	app.product("Data", ["$http",
+		function ($http, $q) {
+			// retrieve api data information for products using api index.php document
+			var serviceProduct = "/backend/php/api/product/index.php";
+
+			var obj = {};
+			// Get object and return $http.get
+			obj.get = function (q) {
+				return $http.get(serviceProduct + q).then(function (results) {
+					return results.data;
+				});
+			};
+			// Post object and return $http.get
+			obj.post = function (q, object) {
+				return $http.post(serviceProduct + q, object).then(function (results) {
+					return results.data;
+				});
+			};
+			// Put object and return $http.get
+			obj.put = function (q, object) {
+				return $http.put(serviceProduct + q, object).then(function (results) {
+					return results.data;
+				});
+			};
+			// Delete object and return results
+			obj.delete = function (q) {
+				return $http.delete(serviceProduct + q).then(function (results) {
+					return results.data;
+				});
+			};
+			return obj;
+		}]);
+
+
+
+
+
+
+
+}
