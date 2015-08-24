02382e80cd79020b4a5a61515d0ce96a08ef8e48
Marie Vigil <mjkvigil@gmail.com>
2015-08-23 22:16:17 -0600

    https://app.asana.com/0/41180108506105/46061359778942
    created Product View file
    
     A view is responsible for rendering a template.
    
    It's recommended to have a helper subclass Set which implements \ArrayAccess, \Countable, \IteratorAggregate

---
+++ public_html/backend/php/api/lib/views/product-view.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Product View
+ *
+ * A view is responsible for rendering a template. The view
+ * should subclass \api\lib\views\product-view and implement this interface:
+ *
+ * public render(string $template);
+ *
+ *
+ * It's recommended to have a subclass Set which implements
+ * \ArrayAccess, \Countable, \IteratorAggregate
+ *
+ */
+
+
+class View {
+
+
+
+
+
+
+
+}
+
