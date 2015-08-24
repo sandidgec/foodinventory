fbc78db9422b3bea9c4d08aaa193440f476381de
James Huber <jhuber8@cmn.edu>
2015-08-24 17:05:50 -0500

    I be blockin docs like woe(service) https://app.asana.com/0/42983347910373/46061359778940

--- public_html/lib/angular/services/notification.js
+++ public_html/lib/angular/services/notification.js
@@ -1,7 +1,11 @@
 app.service("RegisterService", function($http, $q) {
 	this.REGISTER_ENDPOINT = "../../backend/php/api/notification/";
 
-
+	/**
+	 * method that promises to get notifications by notificationId
+	 *
+	 * @return accepts the promise when movements are found, rejected otherwise
+	 **/
 	this.getNotificationByNotificationId = function(notificationId) {
 		return ($http.get(this.REGISTER_ENDPOINT +  notificationId)
 			.then(function(reply) {
@@ -14,7 +18,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+	/**
+	* method that promises to get notifications by alertId
+	*
+	* @return accepts the promise when movements are found, rejected otherwise
+	**/
 	this.getNotificationByAlertId = function(alertId) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?alertId=" + alertId)
 			.then(function(reply) {
@@ -27,6 +35,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	* method that promises to get notifications by emailStatus
+	*
+	* @return accepts the promise when movements are found, rejected otherwise
+	**/
 	this.getNotificationByEmailStatus = function(emailStatus) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?emailStatus=" + emailStatus)
 			.then(function(reply) {
@@ -39,6 +52,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	 * method that promises to get notifications by notificationDateTime
+	 *
+	 * @return accepts the promise when movements are found, rejected otherwise
+	 **/
 	this.getNotificationByNotificationDateTime = function(notificationDateTime) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?notificationDateTime=" + notificationDateTime)
 			.then(function(reply) {
@@ -51,6 +69,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	 * method that promises to get products by alertId
+	 *
+	 * @return accepts the promise when movements are found, rejected otherwise
+	 **/
 	this.getProductByAlertId = function(alertId) {
 		return ($http.get(this.REGISTER_ENDPOINT + "?alertId=" + alertId)
 			.then(function(reply) {
@@ -63,6 +86,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	 * method that promises to get notifications
+	 *
+	 * @return accepts the promise when movements are found, rejected otherwise
+	 **/
 	this.getAllNotifications = function() {
 		return ($http.get(this.REGISTER_ENDPOINT)
 			.then(function(reply) {
@@ -75,7 +103,11 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
-
+/**
+ * method that promises to add a notification
+ *
+ * @return accepts the promise when movement is added, rejected otherwise
+ **/
 	this.addNotification = function(notification) {
 		return ($http.post(this.REGISTER_ENDPOINT, notification)
 			.then(function(reply) {
