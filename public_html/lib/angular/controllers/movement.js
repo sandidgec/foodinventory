2ba7a7271fefb872460742ae14d9868b13c7f900
Topher Collopy <chelco91@gmail.com>
2015-08-26 14:47:53 -0600

    configured controller for movement. https://app.asana.com/0/42983347910373/46061359778938

--- public_html/lib/angular/controllers/movement.js
+++ public_html/lib/angular/controllers/movement.js
@@ -1,18 +1,18 @@
 app.controller("MovementController", function($http, $scope, MovementService) {
-    $scope.movement = null;
-    $scope.statusClass = "alert-success";
-    $scope.statusMessage = null;
+	$scope.movement = null;
+	$scope.statusClass = "alert-success";
+	$scope.statusMessage = null;
 
-    /**
-     *method that controls the action table and will fill the table or display errors
-     */
-    $scope.addMovement = function(movement) {
-        MovementService.addMovement(movement);
-        if(reply.status === 200) {
-            $scope.actions = reply.data;
-        } else {
-            $scope.statusClass = "alert-danger";
-            $scope.statusMessage = reply.message;
-        }
-    };
+	/**
+	 *method that controls the action table and will fill the table or display errors
+	 */
+	$scope.addMovement = function(movement) {
+		MovementService.addMovement(movement);
+		if(reply.status === 200) {
+			$scope.actions = reply.data;
+		} else {
+			$scope.statusClass = "alert-danger";
+			$scope.statusMessage = reply.message;
+		}
+	};
 });
