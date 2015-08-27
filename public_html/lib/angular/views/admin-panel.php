78b71e33bbfd04f95333c5ab1614d202a1b49f84
Topher Collopy <chelco91@gmail.com>
2015-08-27 11:20:30 -0600

    switched css file for admin-panel. https://app.asana.com/0/42983347910373/46061359778938

--- public_html/lib/angular/views/admin-panel.php
+++ public_html/lib/angular/views/admin-panel.php
@@ -15,7 +15,7 @@
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
 
 		<!-- LINK TO YOUR CUSTOM CSS FILES HERE -->
-		<link type="text/css" href="../../css/styles.css" rel="stylesheet"/>
+		<link type="text/css" href="../../css/admin-panel.css" rel="stylesheet"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,16 +26,13 @@
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 		<!-- angular.js -->
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.min.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.route.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-route.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-messages.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min.js"></script>
 		<script type="text/javascript" src="../food-inventory.js"></script>
@@ -52,7 +49,7 @@
 		<script type="text/javascript" src="../directives/movement.js"></script>
 		<script type="text/javascript" src="../directives/vendor.js"></script>
 		<script type="text/javascript" src="../directives/location.js"></script>
-		<script type="text/javascript" src="../directives/notification.js"></script>
+<!--		<script type="text/javascript" src="../directives/notification.js"></script>-->
 
 		<!-- Page Title -->
 		<title>Administration</title>
@@ -60,15 +57,13 @@
 
 	<body>
 		<section class="container">
-			<!--  Admin Panel Header  -->
-			<header>
+			<!--  Admin Panel Sidebar  -->
+			<div class="vertical-text col-md-2">
 				<h1 class="text-center">Admin Panel</h1>
-
-				<h2 class="text-center">Do Your Thing</h2>
-			</header>
+			</div>
 
 			<!-- Admin Tabs  -->
-			<section ng-controller="TabController as tab">
+			<section class="col-md-10" ng-controller="TabController as tab">
 				<ul class="nav nav-pills">
 					<li ng-class="{ active:tab.isSet(1) }">
 						<a href="" ng-click="tab.setTab(1)">Product</a>
