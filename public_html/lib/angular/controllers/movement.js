ebd9aa9c2976416443ccca21a187b6fde6d5f966
ccollopy <chelco91@gmasil.com>
2015-08-26 21:22:48 -0600

    finished writing the rest of the movement controller methods. https://app.asana.com/0/42983347910373/46061359778938

--- public_html/lib/angular/controllers/movement.js
+++ public_html/lib/angular/controllers/movement.js
@@ -4,15 +4,113 @@ app.controller("MovementController", function($http, $scope, MovementService) {
 	$scope.statusMessage = null;
 
 	/**
-	 *method that controls the action table and will fill the table or display errors
+	 * method that controls the action table and will fill the table or display errors
 	 */
 	$scope.addMovement = function(movement) {
-		MovementService.addMovement(movement);
-		if(reply.status === 200) {
-			$scope.actions = reply.data;
-		} else {
-			$scope.statusClass = "alert-danger";
-			$scope.statusMessage = reply.message;
-		}
+		MovementService.addMovement(movement)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByMovementId = function(movementId) {
+		MovementService.getMovementByMovementId(movementId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByFromLocationId = function(fromLocationId) {
+		MovementService.getMovementByFromLocationId(fromLocationId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByToLocationId = function(toLocationId) {
+		MovementService.getMovementByToLocationId(toLocationId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByProductId = function(productId) {
+		MovementService.getMovementByProductId(productId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByUserId = function(userId) {
+		MovementService.getMovementByUserId(userId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByMovementDate = function(movementDate) {
+		MovementService.getMovementByMovementDate(movementDate)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getMovementByMovementType = function(movementType) {
+		MovementService.getMovementByMovementType(movementType)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getAllMovements = function(page) {
+		MovementService.getAllMovements(page)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
 	};
 });
