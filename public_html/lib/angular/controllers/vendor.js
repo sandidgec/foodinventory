02cf3752f0530cc440956365b0488252787e4e61
James Huber <jhuber8@cmn.edu>
2015-08-27 00:35:26 -0500

    services and controllers update for vendor https://app.asana.com/0/41180108506116/46061359778948

---
+++ public_html/lib/angular/controllers/vendor.js
@@ -0,0 +1,92 @@
+app.controller("VendorController", function($http, $scope, VendorService) {
+	$scope.vendor = null;
+	$scope.statusClass = "alert-success";
+	$scope.statusMessage = null;
+
+	/**
+	 * method that controls the action table and will fill the table or display errors
+	 */
+	$scope.addVendor = function(vendor) {
+		VendorService.addVendor(vendor)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.editVendor = function(vendor) {
+		VendorService.editVendor(vendor)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.deleteVendor = function(vendor) {
+		VendorService.deleteVendor(vendor)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getVendorByVendorId = function(vendorId) {
+		VendorService.getVendorByVendorId(vendorId)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getVendorByVendorEmail = function(vendorEmail) {
+		VendorService.getVendorByVendorEmail(vendorEmail)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getVendorByVendorName = function(vendorName) {
+		VendorService.getVendorByVendorName(vendorName)
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+
+	$scope.getAllVendors = function() {
+		VendorService.getAllVendors()
+			.then(function(reply) {
+				if(reply.status === 200) {
+					$scope.actions = reply.data;
+				} else {
+					$scope.statusClass = "alert-danger";
+					$scope.statusMessage = reply.message;
+				}
+			});
+	};
+});
--- public_html/lib/angular/services/vendor.js
+++ public_html/lib/angular/services/vendor.js
@@ -1,81 +1,13 @@
-app.service("RegisterService", function($http, $q) {
-	this.VENDOREDITOR_ENDPOINT = "../../backend/php/api/vendor/";
+app.service("VendorService", function($http, $q) {
+	this.VENDOR_ENDPOINT = "../../backend/php/api/vendor/";
 
-	/**
-	 * method that promises to get vendors by vendorId
-	 *
-	 * @return accepts the promise when vendors are found, rejected otherwise
-	 **/
-	this.getVendors = function(vendorId) {
-		return ($http.get(this.VENDOREDITOR_ENDPOINT +  vendorId)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
-	/**
-	 * method that promises to get vendors by vendorEmail
-	 *
-	 * @return accepts the promise when vendors are found, rejected otherwise
-	 **/
-	this.getVendors = function(vendorEmail) {
-		return ($http.get(this.VENDOREDITOR_ENDPOINT + "?vendorEmail=" + vendorEmail)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
-	/**
-	 * method that promises to get vendors by vendorName
-	 *
-	 * @return accepts the promise when vendors are found, rejected otherwise
-	 **/
-	this.getVendors = function(vendorName) {
-		return ($http.get(this.VENDOREDITOR_ENDPOINT + "?vendorName=" + vendorName)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
-	/**
-	 * method that promises to get vendors
-	 *
-	 * @return accepts the promise when vendors are found, rejected otherwise
-	 **/
-	this.getAllVendors = function() {
-		return ($http.get(this.VENDOREDITOR_ENDPOINT)
-			.then(function(reply) {
-				if(typeof reply.data === "object") {
-					return (reply.data);
-				} else {
-					return ($q.reject(reply.data));
-				}
-			}, function(reply) {
-				return ($q.reject(reply.data));
-			}));
-	};
 	/**
 	 * method that promises to add vendors
 	 *
 	 * @return accepts the promise when vendors are added, rejected otherwise
 	 **/
 	this.addVendor = function(vendor) {
-		return ($http.post(this.VENDOREDITOR_ENDPOINT, vendor)
+		return ($http.post(this.VENDOR_ENDPOINT, vendor)
 			.then(function(reply) {
 				if(typeof reply.data === "object") {
 					return (reply.data);
@@ -92,7 +24,7 @@ app.service("RegisterService", function($http, $q) {
 	 * @return accepts the promise when vendors are edited, rejected otherwise
 	 **/
 	this.editVendor = function(vendor) {
-		return ($http.put(this.VENDOREDITOR_ENDPOINT + vendor.vendorId, vendor)
+		return ($http.put(this.VENDOR_ENDPOINT + vendor.vendorId, vendor)
 			.then(function(reply) {
 				if(typeof reply.data === "object") {
 					return (reply.data);
@@ -108,8 +40,8 @@ app.service("RegisterService", function($http, $q) {
 	 *
 	 * @return accepts the promise when vendors are deleted, rejected otherwise
 	 **/
-	this.destroyVendor = function(user) {
-		return ($http.delete(this.VENDOREDITOR_ENDPOINT + vendor.vendorId)
+	this.deleteVendor = function(vendor) {
+		return ($http.delete(this.VENDOR_ENDPOINT + vendor.vendorId, vendor)
 			.then(function(reply) {
 				if(typeof reply.data === "object") {
 					return (reply.data);
@@ -120,4 +52,73 @@ app.service("RegisterService", function($http, $q) {
 				return ($q.reject(reply.data));
 			}));
 	};
+	/**
+	 * method that promises to get vendors by vendorId
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
+	this.getVendorByVendorId = function(vendorId) {
+		return ($http.get(this.VENDOR_ENDPOINT +  vendorId)
+			.then(function(reply) {
+				if(typeof reply.data === "object") {
+					return (reply.data);
+				} else {
+					return ($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return ($q.reject(reply.data));
+			}));
+	};
+	/**
+	 * method that promises to get vendors by vendorEmail
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
+	this.getVendorByVendorEmail = function(vendorEmail) {
+		return ($http.get(this.VENDOR_ENDPOINT + "?vendorEmail=" + vendorEmail)
+			.then(function(reply) {
+				if(typeof reply.data === "object") {
+					return (reply.data);
+				} else {
+					return ($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return ($q.reject(reply.data));
+			}));
+	};
+	/**
+	 * method that promises to get vendors by vendorName
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
+	this.getVendorByVendorName = function(vendorName) {
+		return ($http.get(this.VENDOR_ENDPOINT + "?vendorName=" + vendorName)
+			.then(function(reply) {
+				if(typeof reply.data === "object") {
+					return (reply.data);
+				} else {
+					return ($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return ($q.reject(reply.data));
+			}));
+	};
+	/**
+	 * method that promises to get vendors
+	 *
+	 * @return accepts the promise when vendors are found, rejected otherwise
+	 **/
+	this.getAllVendors = function() {
+		return ($http.get(this.VENDOR_ENDPOINT)
+			.then(function(reply) {
+				if(typeof reply.data === "object") {
+					return (reply.data);
+				} else {
+					return ($q.reject(reply.data));
+				}
+			}, function(reply) {
+				return ($q.reject(reply.data));
+			}));
+	};
+
 });
