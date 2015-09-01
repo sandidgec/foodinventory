f2823a0961d346ee39402eafb1b171254a24c1ad
Charles Sandidge <sandidgec@gmail.com>
2015-09-01 11:49:03 -0600

    added a JSON reply to logout  https://app.asana.com/0/42983347910373/46061359778936

--- public_html/backend/php/lib/logout.php
+++ public_html/backend/php/lib/logout.php
@@ -3,3 +3,11 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 unset($_SESSION["user"]);
+
+// Angular needs a status object
+$reply = new stdClass();
+$reply->status = 200;
+$reply->message = "you are now logged out";
+
+header("Content-type: application/json");
+echo json_encode($reply);
