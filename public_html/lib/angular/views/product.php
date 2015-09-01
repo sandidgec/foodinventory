7a1f2c596b991b37bcde3327c2839c512f7298b1
Christopher Collopy <chelco91@gmail.com>
2015-09-01 10:53:37 -0600

    moved edit and delete buttons into their own columns in the table, and added a search box to the view. https://app.asana.com/0/42983347910373/46061359778942

--- public_html/lib/angular/views/product.php
+++ public_html/lib/angular/views/product.php
@@ -9,6 +9,12 @@
 				<i class="fa fa-plus fa-2x"></i>
 			</a>
 		</div>
+		<div class="col-md-5 col-md-offset-4">
+			<label for="search" class="col-sm-2 control-label">Search: </label>
+			<div class="col-sm-8 col-sm-offset-2">
+				<input type="text" class="form-control" id="search" name="search" placeholder="Search Stuff Here" />
+			</div>
+		</div>
 	</div>
 
 	<!--  Product Reports -->
@@ -24,6 +30,8 @@
 						<th>Vendor</th>
 						<th>SKU</th>
 						<th>Lead Time</th>
+						<th class="center"><i class="fa fa-pencil fa-x"></i></th>
+						<th class="center"><i class="fa fa-trash fa-x"></i></th>
 					</tr>
 				</thead>
 
@@ -35,16 +43,14 @@
 						<td>{{ product.sku }}</td>
 						<td>{{ product.leadTime }}</td>
 						<td>
-							<div class="col-sm-6">
-								<a href="#" class="btn btn-md btn-info" data-toggle="modal" data-target="#EditProductModal">
-									<i class="fa fa-pencil"></i>
-								</a>
-							</div>
-							<div class="col-sm-6">
-								<form ng-submit="deleteProduct(product.productId);">
-									<button type="submit" class="btn btn-md btn-danger"><i class="fa fa-trash"></i></button>
-								</form>
-							</div>
+							<a href="#" class="btn btn-md btn-info" data-toggle="modal" data-target="#EditProductModal">
+								<i class="fa fa-pencil"></i>
+							</a>
+						</td>
+						<td>
+							<form ng-submit="deleteProduct(product.productId);">
+								<button type="submit" class="btn btn-md btn-danger"><i class="fa fa-trash"></i></button>
+							</form>
 						</td>
 					</tr>
 				</tbody>
@@ -108,66 +114,6 @@
 
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Edit Product Modal -->
-	<div class="modal fade" id="EditProductModal">
-		<div class="modal-dialog">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-					<h3 class="modal-title">Edit a Product</h3>
-				</div>
-
-				<div class="modal-body" ng-controller="ProductController">
-					<form class="form-horizontal" ng-submit="addProduct(product);">
-						<div class="form-group">
-							<label for="product" class="col-sm-3 control-label">Product:</label>
-							<div class="col-sm-9">
-								<input type="text" class="form-control" id="product" name="product" placeholder="Enter Product " ng-model="product.title"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="Description" class="col-sm-3 control-label">Description</label>
-							<div class="col-sm-9">
-								<input type="text" class="form-control" id="description" name="description" placeholder="Enter Product Description" ng-model="product.description"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="vendor-search" class="col-sm-3 control-label">Vendor</label>
-							<div class="col-sm-8">
-								<input type="text" class="form-control" id="vendor-search" name="vendor-search" placeholder="Enter Vendor"
-										 ng-model="product.vendorId" typeahead="vendor for vendorName in getVendorByVendorName($viewValue)"
-										 typeahead-loading="loadingVendors" typeahead-no-results="noResults"/>
-								<i ng-show="loadingVendors" class="glyphicon glyphicon-refresh"></i>
-								<div ng-show="noResults">
-									<i class="glyphicon glyphicon-remove"></i>No Results Found
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="sku" class="col-sm-3 control-label">SKU:</label>
-							<div class="col-sm-9">
-								<input type="text" class="form-control" id="sku" name="sku" placeholder="Enter SKU " ng-model="product.sku"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="leadTime" class="col-sm-3 control-label">Lead Time:</label>
-							<div class="col-sm-9">
-								<input type="text" class="form-control" id="leadTime" name="leadTime" placeholder="Enter Order Lead Time" ng-model="product.leadTime"/>
-							</div>
-						</div>
-						<pre>form = {{ product | json }}</pre>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Submit</button>
-						</div>
-					</form>
 				</div>
 			</div>
 		</div>
