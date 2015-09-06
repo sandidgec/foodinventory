992f210639cbab8229ebfc5be9fdca16940c7c87
Charles Sandidge <sandidgec@gmail.com>
2015-09-05 18:50:08 -0600

    added image to launch page   https://app.asana.com/0/42983347910373/46061359778936

--- public_html/pages/launch-page.php
+++ public_html/pages/launch-page.php
@@ -13,32 +13,39 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	</head>
 	<body class="site">
 		<div class="site-content">
-			<div class="gradient">
-				<header>
-					<?php require_once "header.php"; ?>
-				</header>
 
-				<div class="placeholder"></div>
-				<h2 class="welcome">Welcome your new employee.</h2>
-				<img class="center-block" src="../lib/images/invtextboxlogo.png"  alt="Logo Image">
-			</div>
-			<main class="home">
+			<header>
+				<?php require_once "header.php"; ?>
+			</header>
 
-				<section class="col-xs-3 leftgraphic text-center">
-				</section>
+			<img class="center-block" src="../lib/images/frontimage.png" alt="Logo Image">
 
-				<section class="col-xs-6 testimonials text-center">
-					<h3>Testimonials</h3>
-					<p class="test">
-						"This is inventory management done right, and done better!" - John Smith
-					</p>
-				</section>
-
-				<section class="col-xs-3 rightgraphic text-center">
-				</section>
-
-			</main>
+			<h2 class="welcome">Welcome your new employee.</h2>
 		</div>
+		<main class="home">
+
+			<section class="col-xs-3 leftgraphic text-center">
+				<p class="outtests">
+					"Being a small business owner, I've tried every inventory management software out there. Inventory Text is the easiest and most complete package I've come
+					across. 10 out of 10!" - Dylan McDonald
+				</p>
+			</section>
+
+			<section class="col-xs-6 testimonials text-center">
+				<h3>Testimonials</h3>
+
+				<p class="test">
+					"This is inventory management done right, and done better! Ease of use and a simple interface made me choose Inventory Text." - John Smith
+				</p>
+			</section>
+
+			<section class="col-xs-3 rightgraphic text-center">
+				<p class="outtests">
+					"There's a million inventory management products, but Inventory Text really acts like an extra employee." - Anne Miller
+				</p>
+			</section>
+
+		</main>
 		<footer>
 			<?php require_once "footer.php"; ?>
 		</footer>
